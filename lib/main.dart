b3567001a87cebb331ad3dd334aa27075ddd8f4e
Abdulloh-Abdullayev <abdullohabdullayev321@gmail.com>
2021-12-21 12:40:48 +0200

    ff

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // ssss
+        // jkhkjhkjhkjgkjghkjhkjhkjhkk
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(),
